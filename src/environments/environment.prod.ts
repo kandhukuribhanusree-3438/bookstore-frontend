101b236f53de194d5157f46d46543c55a2ff3be4
kandhukuribhanusree-3438 <bhanusreekandhukuri03@gmail.com>
2025-11-02 14:04:24 +0530

    push

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://your-backend-render-url.onrender.com/api'
+  apiUrl: 'https://bookstore-gu1p.onrender.com'
 };
 
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: false,
-  apiUrl: 'https://bookstore-gu1p.onrender.com/api'
+  apiUrl: 'https://bookstore-gu1p.onrender.com'
 };
